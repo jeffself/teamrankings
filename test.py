5f3cee1784f68207fc2d6b58e26648c739a23ac8
S.Lott <slott56@gmail.com>
2012-08-17 10:45:09 -0400

    Update tests to show examples of additional kinds of unit testing that might be helpful.

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
+"""Team Rankings unit test case(s).
+
+These are just two super-simplistic unit test cases
+for the rankings.py application.
+
+..  todo:: Write more tests: Team, Game, etc.
+"""
 import unittest
 import sys
 import io
 
 import rankings
-
 
 class Test_Reports( unittest.TestCase ):
     def setUp( self ):
@@ -33,6 +39,51 @@ class Test_Reports( unittest.TestCase ):
             for e, a in  zip( (l.rstrip() for l in captured.readlines()),
                     output_report.getvalue().splitlines() ):
                 self.assertEqual( e, a )
+
+class TestSportFactor( unittest.TestCase ):
+    def setUp( self ):
+        self.sf= rankings.SportFactor()
+    def test_should_normalize_score( self ):
+        self.assertAlmostEqual( 1-(1**2)/100, self.sf.adjustScore( 1 ) )
+        self.assertAlmostEqual( 6-(6**2)/100, self.sf.adjustScore( 6 ) )
+        self.assertAlmostEqual( 12-(12**2)/100, self.sf.adjustScore( 12 ) )
+    def test_should_compute_game_ratio( self ):
+        a1, a2 = (1-(1**2)/100)**2, (2-(2**2)/100)**2
+        self.assertAlmostEqual( (a1+1)/(a1+a2+2)/2, self.sf.gameRatio(1,2) )
+        a1, a2 = (2-(2**2)/100)**2, (2-(2**2)/100)**2
+        self.assertAlmostEqual( ((a1+1)/(a1+a2+2)+.5)/2, self.sf.gameRatio(2,2) )
+        a1, a2 = (2-(2**2)/100)**2, (1-(1**2)/100)**2,
+        self.assertAlmostEqual( ((a1+1)/(a1+a2+2)+1)/2, self.sf.gameRatio(2,1) )
+
+class TestFootballSportFactor( unittest.TestCase ):
+    def setUp( self ):
+        self.sf= rankings.Football()
+    def test_should_normalize_score( self ):
+        self.assertAlmostEqual( 1-(1**2)/250, self.sf.adjustScore( 1 ) )
+        self.assertAlmostEqual( 6-(6**2)/250, self.sf.adjustScore( 6 ) )
+        self.assertAlmostEqual( 12-(12**2)/250, self.sf.adjustScore( 12 ) )
+    def test_should_compute_game_ratio( self ):
+        a1, a2 = ((1-(1**2)/250)/6)**2, ((2-(2**2)/250)/6)**2
+        self.assertAlmostEqual( (a1+1)/(a1+a2+2)/2, self.sf.gameRatio(1,2) )
+        a1, a2 = ((2-(2**2)/250)/6)**2, ((2-(2**2)/250)/6)**2
+        self.assertAlmostEqual( ((a1+1)/(a1+a2+2)+.5)/2, self.sf.gameRatio(2,2) )
+        a1, a2 = ((2-(2**2)/250)/6)**2, ((1-(1**2)/250)/6)**2,
+        self.assertAlmostEqual( ((a1+1)/(a1+a2+2)+1)/2, self.sf.gameRatio(2,1) )
+
+class TestBasketballSportFactor( unittest.TestCase ):
+    def setUp( self ):
+        self.sf= rankings.Basketball()
+    def test_should_normalize_score( self ):
+        self.assertAlmostEqual( 80-(80**2)/750, self.sf.adjustScore( 80 ) )
+        self.assertAlmostEqual( 90-(90**2)/750, self.sf.adjustScore( 90 ) )
+        self.assertAlmostEqual( 100-(100**2)/750, self.sf.adjustScore( 100 ) )
+    def test_should_compute_game_ratio( self ):
+        a1, a2 = ((80-(80**2)/750)/3)**2, ((90-(90**2)/750)/3)**2
+        self.assertAlmostEqual( (a1+1)/(a1+a2+2)/2, self.sf.gameRatio(80,90) )
+        a1, a2 = ((80-(80**2)/750)/3)**2, ((80-(80**2)/750)/3)**2
+        self.assertAlmostEqual( ((a1+1)/(a1+a2+2)+.5)/2, self.sf.gameRatio(80,80) )
+        a1, a2 = ((90-(90**2)/750)/3)**2, ((80-(80**2)/750)/3)**2
+        self.assertAlmostEqual( ((a1+1)/(a1+a2+2)+1)/2, self.sf.gameRatio(90,80) )
 
 if __name__ == "__main__":
     unittest.main()
